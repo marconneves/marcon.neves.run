9a0e333cfcbd208cf3d1d402c5fc47efbb517b7e
Marcon Neves <marcon@codgital.com>
2025-10-08 18:41:48 +0000

    fix: base do outro

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { resolve } from 'node:path'
 
 import tailwindcss from '@tailwindcss/vite'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -9,15 +8,10 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    nitroV2Plugin(),
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
