46c0d0103e051cd7d111a2640fba2300a46e9f00
Marcon Neves <marcon@codgital.com>
2025-10-10 12:23:28 +0000

    fix: atualizar index base

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import MenuItems from "./MenuItems";
 
 export default function Header() {
@@ -5,7 +6,7 @@ export default function Header() {
   return (
     <header className="py-8 px-10 flex justify-between font-sans text-white shadow-lg absolute w-full flex-wrap">
       <h2 className="hidden md:block ml-4 text-xl font-bold">
-        Marcon Neves
+        <Link href='/' prefetch>Marcon Neves</Link>
       </h2>
       <h3 className="block md:hidden ml-4 text-xl font-bold">
         M
