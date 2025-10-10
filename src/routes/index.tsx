19eae0e64d94112621c33d990af94d0aeda4db0f
Marcon Neves <marcon@codgital.com>
2025-10-10 01:48:55 +0000

    fix: atualizar para tanstack start

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-import HomeScreen from '@/features/home/screen/HomeScreen'
+import HomeScreen from '~/features/home/screen/HomeScreen'
 
 export const Route = createFileRoute('/')({
   component: HomeScreen,
